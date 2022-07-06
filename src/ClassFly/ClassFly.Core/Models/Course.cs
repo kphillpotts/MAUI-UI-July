d37ec668a5fb7fc85168fafcdfe2a2cc2e362a3f
Lachlan Gordon <lachlanwgordon@gmail.com>
2022-07-07 09:22:00 +1000

    Added Core project and models

---
+++ src/ClassFly/ClassFly.Core/Models/Course.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassFly.Core.Models;
+public class Course
+{
+    public string Name { get; set; }
+    public string Image { get; set; }
+    public int MemberCount { get; set; }
+
+    public List<Mentor> Mentors { get; set; }
+
+}
---
+++ src/ClassFly/ClassFly.Core/Models/DataService.cs
@@ -0,0 +1,38 @@
+﻿namespace ClassFly.Core.Models;
+
+public class DataService
+{
+    public static List<Course> GetCourses() => new()
+    {
+        new Course{Name = "Guitar", Mentors = new ()
+        {
+            new Mentor {Name = "Scarlett", FollowerCount = 70},
+            new Mentor {Name = "Jhony", FollowerCount = 150},
+            new Mentor {Name = "Linda", FollowerCount = 30},
+        }},
+        new Course{Name = "Saxophone", Mentors = new ()
+        {
+            new Mentor {Name = "Scarlett", FollowerCount = 70},
+            new Mentor {Name = "Jhony", FollowerCount = 150},
+            new Mentor {Name = "Linda", FollowerCount = 30},
+        }},
+        new Course{Name = "Drums", Mentors = new ()
+        {
+            new Mentor {Name = "Scarlett", FollowerCount = 70},
+            new Mentor {Name = "Jhony", FollowerCount = 150},
+            new Mentor {Name = "Linda", FollowerCount = 30},
+        }},
+        new Course{Name = "Piano", Mentors = new ()
+        {
+            new Mentor {Name = "Scarlett", FollowerCount = 70},
+            new Mentor {Name = "Jhony", FollowerCount = 150},
+            new Mentor {Name = "Linda", FollowerCount = 30},
+        }},
+        new Course{Name = "Double Bass", Mentors = new ()
+        {
+            new Mentor {Name = "Scarlett", FollowerCount = 70},
+            new Mentor {Name = "Jhony", FollowerCount = 150},
+            new Mentor {Name = "Linda", FollowerCount = 30},
+        }},
+    };
+}
